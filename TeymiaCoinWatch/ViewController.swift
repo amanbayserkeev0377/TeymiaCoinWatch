49bd796888c78e2c64a8988e62689208df9cdc28
amanbayserkeev0377 <amanbayserkeev0377@gmail.com>
2026-02-14 16:33:53 +0600

    Initial Commit

---
+++ TeymiaCoinWatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TeymiaCoinWatch
+//
+//  Created by Aman Bayserkeev on 14/2/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
